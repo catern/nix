ebde5c324fc1705ccc9a4e9ce76138b99d222530
Spencer Baugh <sbaugh@catern.com>
2018-01-19 03:59:45 +0000

    wip stuff

--- nar.py
+++ nar.py
@@ -3,53 +3,53 @@ import construct
 
 example = b'\r\x00\x00\x00\x00\x00\x00\x00nix-archive-1\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00(\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00type\x00\x00\x00\x00\x07\x00\x00\x00\x00\x00\x00\x00regular\x00\x08\x00\x00\x00\x00\x00\x00\x00contentsO\x00\x00\x00\x00\x00\x00\x00with (import <nixpkgs>) {};\nrunCommand "foo" {}\n\'\'\necho hi\necho boop > $out\n\'\'\n\x00\x01\x00\x00\x00\x00\x00\x00\x00)\x00\x00\x00\x00\x00\x00\x00'
 
-nixstr = construct.Aligned(8, construct.PascalString(construct.Int64ul, encoding="utf8"))
+nixstr = construct.Aligned(8, construct.PascalString(construct.Int64ul))
 
 regular = construct.Struct(
-    construct.Const(nixstr, "type"),
-    "type" / construct.Const(nixstr, "regular"),
-    construct.Const(nixstr, "contents"),
+    construct.Const(nixstr, b"type"),
+    "type" / construct.Const(nixstr, b"regular"),
+    construct.Const(nixstr, b"contents"),
     "contents" / nixstr
 )
 
 executable = construct.Struct(
-    construct.Const(nixstr, "type"),
-    construct.Const(nixstr, "regular"),
-    "type" / construct.Const(nixstr, "executable"),
-    construct.Const(nixstr, ""),
-    construct.Const(nixstr, "contents"),
+    construct.Const(nixstr, b"type"),
+    construct.Const(nixstr, b"regular"),
+    "type" / construct.Const(nixstr, b"executable"),
+    construct.Const(nixstr, b""),
+    construct.Const(nixstr, b"contents"),
     "contents" / nixstr
 )
 
 symlink = construct.Struct(
-    construct.Const(nixstr, "type"),
-    "type" / construct.Const(nixstr, "symlink"),
-    construct.Const(nixstr, "target"),
+    construct.Const(nixstr, b"type"),
+    "type" / construct.Const(nixstr, b"symlink"),
+    construct.Const(nixstr, b"target"),
     "target" / nixstr
 )
 
 entry = construct.Struct(
-    construct.Const(nixstr, "entry"),
-    construct.Const(nixstr, "("),
-    construct.Const(nixstr, "name"),
+    construct.Const(nixstr, b"entry"),
+    construct.Const(nixstr, b"("),
+    construct.Const(nixstr, b"name"),
     "name" / nixstr,
     "node" / construct.LazyBound(lambda ctx: value),
-    construct.Const(nixstr, ")")
+    construct.Const(nixstr, b")")
 )
 
 directory = construct.Struct(
-    construct.Const(nixstr, "type"),
-    "type" / construct.Const(nixstr, "directory"),
+    construct.Const(nixstr, b"type"),
+    "type" / construct.Const(nixstr, b"directory"),
     "entries" / construct.GreedyRange(entry)
 )
 
 value = construct.Struct(
-    construct.Const(nixstr, "("),
+    construct.Const(nixstr, b"("),
     construct.Embedded(construct.Select(regular, executable, symlink, directory)),
-    construct.Const(nixstr, ")")
+    construct.Const(nixstr, b")")
 )
 
 dump = construct.Struct(
-    construct.Const(nixstr, "nix-archive-1"),
+    construct.Const(nixstr, b"nix-archive-1"),
     construct.Embedded(value)
 )
